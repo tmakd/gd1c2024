f1042b51f86f4ead95174a25a485a5caedc31708
tmakd <ramireztomas91@gmail.com>
2024-05-30 00:36:54 -0300

    fix run .bat

--- run.bat
+++ run.bat
@@ -1,4 +1,1 @@
 sqlcmd -S DESKTOP-86EVMU8\SQLEXPRESS02 -i migrations/create_db.sql, migrations/create_ddl_sp.sql, migrations/create_dml_sp.sql, migrations/migrate_ddl.sql, migrations/migrate_dml.sql -a 32767 -o resultado_output.txt
-
-
-sqlcmd -S DESKTOP-86EVMU8\SQLEXPRESS02 -i run.sql -a 32767 -o resultado_output_unificado.txt
