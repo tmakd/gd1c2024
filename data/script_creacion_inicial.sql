fd45e47e944002136a210f8b7bbdaa118d345d6a
tmakd <ramireztomas91@gmail.com>
2024-06-29 11:57:39 -0300

    fix fact_cliente

--- data/script_creacion_inicial.sql
+++ data/script_creacion_inicial.sql
@@ -414,7 +414,7 @@ FOREIGN KEY (apli_descuento_codigo_descuento) REFERENCES [GeDeDe].Descuento(desc
 END;
 GO
 
-CREATE PROCEDURE [GeDeDe].[CREATE_DML]
+create PROCEDURE [GeDeDe].[CREATE_DML]
 AS
 BEGIN
 	print 'Cargando tabla Super'
@@ -573,13 +573,21 @@ BEGIN
 
     END;
 	print 'Cargando tabla Factura'
+	
     BEGIN
+		SELECT TICKET_TIPO_COMPROBANTE, TICKET_NUMERO, [CLIENTE_DNI], SUCURSAL_NOMBRE into #Ticket_Cliente
+		FROM [GD1C2024].[gd_esquema].[Maestra]
+		WHERE [CLIENTE_DNI] IS NOT NULL AND [TICKET_TIPO_COMPROBANTE] IS NOT NULL AND [TICKET_NUMERO] IS NOT NULL
+
         INSERT INTO [GD1C2024].[GeDeDe].[Factura] (fact_nro, fact_sucursal, fact_caja, fact_vendedor, fact_cliente, fact_fecha_hora, fact_tipo, fact_subtotal_productos, fact_total_descuento_aplicado, fact_descuento_aplicado_mp, fact_total_envio, fact_total_ticket)
         SELECT distinct [TICKET_NUMERO] -- fact_nro
         	  ,[GD1C2024].[GeDeDe].Sucursal.sucu_codigo -- fact_sucursal
         	  ,[CAJA_NUMERO]	-- fact_caja
         	  ,[GD1C2024].[GeDeDe].Empleado.empl_codigo	-- fact_vendedor
-			  ,(SELECT c.clie_codigo FROM [GD1C2024].[GeDeDe].[Cliente] c where c.clie_dni = [CLIENTE_DNI])
+			  ,(SELECT c.clie_codigo 
+			  FROM #Ticket_Cliente tc
+				JOIN [GD1C2024].[GeDeDe].[Cliente] c on c.clie_dni = tc.CLIENTE_DNI
+			  WHERE tc.TICKET_TIPO_COMPROBANTE = m1.TICKET_TIPO_COMPROBANTE and tc.TICKET_NUMERO = m1.TICKET_NUMERO and tc.SUCURSAL_NOMBRE = m1.SUCURSAL_NOMBRE)
               ,[TICKET_FECHA_HORA] -- fact_fecha_hora
               ,[TICKET_TIPO_COMPROBANTE] -- fact_tipo
               ,[TICKET_SUBTOTAL_PRODUCTOS] -- fact_subtotal_productos
@@ -588,7 +596,7 @@ BEGIN
               ,[TICKET_TOTAL_ENVIO] -- fact_total_envio
               ,[TICKET_TOTAL_TICKET] -- fact_total_ticket
 
-        	   FROM [GD1C2024].[gd_esquema].[Maestra]
+        	   FROM [GD1C2024].[gd_esquema].[Maestra] m1
         	   inner join [GD1C2024].[GeDeDe].Empleado on [EMPLEADO_DNI] = Empleado.empl_dni
         	   inner join [GD1C2024].[GeDeDe].Sucursal on [SUCURSAL_NOMBRE] = Sucursal.sucu_nombre
 
